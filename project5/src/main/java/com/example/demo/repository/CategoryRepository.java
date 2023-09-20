41fade287fe59cd1731a92ffb6c78cea03424e5e
truongnx782 <119341472+truongnx782@users.noreply.github.com>
2023-09-20 14:31:28 +0700

    Delete project5 directory

--- project5/src/main/java/com/example/demo/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.Category;
-
-public interface CategoryRepository extends CrudRepository<Category, String>{
-
-}
